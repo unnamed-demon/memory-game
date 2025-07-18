f43aeeb5cb548686e867281a28d1e39d530b63cc
ameermashruf <ameermashruf@gmail.com>
2025-07-18 20:24:24 +0000

    add ui improvements and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Tile from "./components/Tile";
+import LoadingOverlay from "./components/LoadingOverlay";
 import "./App.css";
 
 function genUnclicked() {
   const set = new Set();
-  for(let i=1; i<=150; i++) {
+  for (let i = 1; i <= 150; i++) {
     set.add(i);
   }
   return set;
@@ -12,35 +13,78 @@ function genUnclicked() {
 
 function App() {
   const [unclicked, setUnclicked] = useState(genUnclicked());
-  
+  const [gameOver, setGameOver] = useState(false);
+  const [fetched, setFetched] = useState(0);
+
   const score = 150 - unclicked.size;
   let dice = Math.floor(Math.random() * 4);
+
+  function reset() {
+    setFetched(0);
+    setUnclicked(genUnclicked());
+    setGameOver(false);
+    console.log("new game");
+  }
 
   return (
     <>
       <header>
         <h1>Unnamed Demon's Memory Game</h1>
+        <div className="score-container">          
+          <h1>Score : {String(150 - unclicked.size).padStart(3, "0")}</h1>
+        </div>
       </header>
+
+      <dialog open={gameOver}>
+        {score === 150 ? (
+          <>
+            <b>YOU WIN!</b> <p>Your score is {score}</p>
+          </>
+        ) : (
+          <>
+            <b>GAME OVER</b> <p>Your score is {score}</p>
+          </>
+        )}
+        <button onClick={reset}>Retry</button>
+      </dialog>
+
       <div className="grid-container">
+        {fetched < 4 && <LoadingOverlay />}
         <Tile
           tileType={dice === 0 ? "unique" : "random"}
           unclicked={unclicked}
           setUnclicked={setUnclicked}
+          gameOver={gameOver}
+          setGameOver={setGameOver}
+          fetched={fetched}
+          setFetched={setFetched}
         />
         <Tile
           tileType={dice === 1 ? "unique" : "random"}
           unclicked={unclicked}
           setUnclicked={setUnclicked}
+          gameOver={gameOver}
+          setGameOver={setGameOver}
+          fetched={fetched}
+          setFetched={setFetched}
         />
         <Tile
           tileType={dice === 2 ? "unique" : "random"}
           unclicked={unclicked}
           setUnclicked={setUnclicked}
+          gameOver={gameOver}
+          setGameOver={setGameOver}
+          fetched={fetched}
+          setFetched={setFetched}
         />
         <Tile
           tileType={dice === 3 ? "unique" : "random"}
           unclicked={unclicked}
           setUnclicked={setUnclicked}
+          gameOver={gameOver}
+          setGameOver={setGameOver}
+          fetched={fetched}
+          setFetched={setFetched}
         />
       </div>
     </>
--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -1,84 +1,10 @@
-import { useEffect, useState } from "react";
-import "../App.css";
-
 const loadingImg =
   "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3C!-- Icon from SVG Spinners by Utkarsh Verma - https://github.com/n3r4zzurr0/svg-spinners/blob/main/LICENSE --%3E%3Crect width='7.33' height='7.33' x='1' y='1' fill='currentColor'%3E%3Canimate id='svgSpinnersBlocksWave0' attributeName='x' begin='0;svgSpinnersBlocksWave1.end+0.2s' dur='0.6s' values='1;4;1'/%3E%3Canimate attributeName='y' begin='0;svgSpinnersBlocksWave1.end+0.2s' dur='0.6s' values='1;4;1'/%3E%3Canimate attributeName='width' begin='0;svgSpinnersBlocksWave1.end+0.2s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='0;svgSpinnersBlocksWave1.end+0.2s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3Crect width='7.33' height='7.33' x='8.33' y='1' fill='currentColor'%3E%3Canimate attributeName='x' begin='svgSpinnersBlocksWave0.begin+0.1s' dur='0.6s' values='8.33;11.33;8.33'/%3E%3Canimate attributeName='y' begin='svgSpinnersBlocksWave0.begin+0.1s' dur='0.6s' values='1;4;1'/%3E%3Canimate attributeName='width' begin='svgSpinnersBlocksWave0.begin+0.1s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='svgSpinnersBlocksWave0.begin+0.1s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3Crect width='7.33' height='7.33' x='1' y='8.33' fill='currentColor'%3E%3Canimate attributeName='x' begin='svgSpinnersBlocksWave0.begin+0.1s' dur='0.6s' values='1;4;1'/%3E%3Canimate attributeName='y' begin='svgSpinnersBlocksWave0.begin+0.1s' dur='0.6s' values='8.33;11.33;8.33'/%3E%3Canimate attributeName='width' begin='svgSpinnersBlocksWave0.begin+0.1s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='svgSpinnersBlocksWave0.begin+0.1s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3Crect width='7.33' height='7.33' x='15.66' y='1' fill='currentColor'%3E%3Canimate attributeName='x' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='15.66;18.66;15.66'/%3E%3Canimate attributeName='y' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='1;4;1'/%3E%3Canimate attributeName='width' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3Crect width='7.33' height='7.33' x='8.33' y='8.33' fill='currentColor'%3E%3Canimate attributeName='x' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='8.33;11.33;8.33'/%3E%3Canimate attributeName='y' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='8.33;11.33;8.33'/%3E%3Canimate attributeName='width' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3Crect width='7.33' height='7.33' x='1' y='15.66' fill='currentColor'%3E%3Canimate attributeName='x' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='1;4;1'/%3E%3Canimate attributeName='y' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='15.66;18.66;15.66'/%3E%3Canimate attributeName='width' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='svgSpinnersBlocksWave0.begin+0.2s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3Crect width='7.33' height='7.33' x='15.66' y='8.33' fill='currentColor'%3E%3Canimate attributeName='x' begin='svgSpinnersBlocksWave0.begin+0.3s' dur='0.6s' values='15.66;18.66;15.66'/%3E%3Canimate attributeName='y' begin='svgSpinnersBlocksWave0.begin+0.3s' dur='0.6s' values='8.33;11.33;8.33'/%3E%3Canimate attributeName='width' begin='svgSpinnersBlocksWave0.begin+0.3s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='svgSpinnersBlocksWave0.begin+0.3s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3Crect width='7.33' height='7.33' x='8.33' y='15.66' fill='currentColor'%3E%3Canimate attributeName='x' begin='svgSpinnersBlocksWave0.begin+0.3s' dur='0.6s' values='8.33;11.33;8.33'/%3E%3Canimate attributeName='y' begin='svgSpinnersBlocksWave0.begin+0.3s' dur='0.6s' values='15.66;18.66;15.66'/%3E%3Canimate attributeName='width' begin='svgSpinnersBlocksWave0.begin+0.3s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='svgSpinnersBlocksWave0.begin+0.3s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3Crect width='7.33' height='7.33' x='15.66' y='15.66' fill='currentColor'%3E%3Canimate id='svgSpinnersBlocksWave1' attributeName='x' begin='svgSpinnersBlocksWave0.begin+0.4s' dur='0.6s' values='15.66;18.66;15.66'/%3E%3Canimate attributeName='y' begin='svgSpinnersBlocksWave0.begin+0.4s' dur='0.6s' values='15.66;18.66;15.66'/%3E%3Canimate attributeName='width' begin='svgSpinnersBlocksWave0.begin+0.4s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3Canimate attributeName='height' begin='svgSpinnersBlocksWave0.begin+0.4s' dur='0.6s' values='7.33;1.33;7.33'/%3E%3C/rect%3E%3C/svg%3E";
 
-function genPokenum(tileType, unclicked) {
-  let pokenum = -1;
-
-  if (tileType === "unique") {
-    const unclickedArray = [...unclicked];
-    pokenum = unclickedArray[Math.floor(Math.random() * unclickedArray.length)];
-  } else if (tileType === "random") {
-    pokenum = 1 + Math.floor(Math.random() * 150);
-  }
-
-  return pokenum;
+function LoadingOverlay() {
+  return <div className="loading-overlay">
+    <img src={loadingImg} alt="loading" />
+  </div>;
 }
 
-function Tile({
-  tileType,
-  unclicked,
-  setUnclicked,
-  gameOver,
-  setGameOver,
-  fetched,
-  setFetched,
-}) {
-  const [imgUrl, setImgUrl] = useState(loadingImg);
-  const [pokenum, setPokenum] = useState(genPokenum(tileType, unclicked));
-
-  useEffect(() => {
-    async function fetchPokemon() {
-      try {
-        const tempPokenum = genPokenum(tileType, unclicked);
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon/" + tempPokenum
-        );
-        setTimeout(() => setFetched((fetched) => fetched + 1), 1000);
-        const obj = await response.json();
-        setImgUrl(obj.sprites.front_default);
-        setPokenum(tempPokenum);
-      } catch {
-        console.log("fetch failed");
-        setImgUrl(loadingImg);
-        setPokenum(-1);
-      }
-    }
-    fetchPokemon();
-  }, [unclicked]);
-
-  console.log(fetched);
-
-  return (
-    <div className="grid-tile">
-      <img
-        src={imgUrl}
-        alt="pokemon"
-        data-pokenum={pokenum}
-        onClick={(e) => {
-          if (pokenum == -1) return;
-          if (gameOver) return;
-
-          console.log(pokenum);
-          const tempUnclicked = new Set(unclicked);
-          const didDeleteHappen = tempUnclicked.delete(
-            Number(e.target.dataset.pokenum)
-          );
-
-          if (!didDeleteHappen) setGameOver(true);
-          else if (tempUnclicked.size === 0) {
-            setUnclicked(tempUnclicked);
-            setGameOver(true);
-          } else {
-            setUnclicked(tempUnclicked);
-            setFetched(0);
-          }
-        }}
-      />
-    </div>
-  );
-}
-
-export default Tile;
+export default LoadingOverlay;
